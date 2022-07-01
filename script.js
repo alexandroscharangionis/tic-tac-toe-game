d22199c7055623534d14794f05ee7a0024fdbf9b
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-07-01 13:06:31 +0300

    created gameBoar dController module that return function for actual display of X/O depending on c urrent active player

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Module that generates an object with the gameboard. Each square has an assigned index number.
-const gameBoard = (() => {
+const gameBoardMap = (() => {
   const assignedSquares = {};
   for (let i = 0; i < 9; i++) {
     assignedSquares[i.toString()] = i;
@@ -12,7 +12,7 @@ const gameBoard = (() => {
 // Factory function that generates a Player object with name, symbol (X or O) and methods.
 const Player = (playerName, playerSymbol) => {
   function addMoveToBoard(position) {
-    gameBoard.assignedSquares[position.toString()] = playerSymbol;
+    gameBoardMap.assignedSquares[position.toString()] = playerSymbol;
   }
 
   return { playerName, playerSymbol, addMoveToBoard };
@@ -22,6 +22,7 @@ const Player = (playerName, playerSymbol) => {
 
 const startGameModule = (() => {
   let players = [];
+  let activePlayer = [];
   function startGame(event) {
     event.preventDefault();
     const errorMsg = document.querySelector(".error");
@@ -36,7 +37,7 @@ const startGameModule = (() => {
       playerO = Player(`${inputPlayerO}`, "O");
       players.push(playerX);
       players.push(playerO);
-
+      activePlayer.push(players[0]);
       gameMenu.style.display = "none";
       boardContainer.style.display = "block";
       console.log(playerX, playerO);
@@ -45,8 +46,32 @@ const startGameModule = (() => {
     }
   }
 
-  return { startGame, players };
+  return { startGame, players, activePlayer };
 })();
 
 const startGameBtn = document.getElementById("startGame");
 startGameBtn.addEventListener("click", startGameModule.startGame);
+
+// Module that returns a function that displays X/O on gameboard depending on current active player, and then updates the active player
+const gameBoardController = (() => {
+  function playerMove(event) {
+    if (startGameModule.activePlayer[0] === startGameModule.players[0]) {
+      startGameModule.players[0].addMoveToBoard(`${event.target.id}`);
+      event.target.textContent = startGameModule.players[0].playerSymbol;
+      startGameModule.activePlayer[0] = startGameModule.players[1];
+      // console.log(gameBoardMap.assignedSquares, startGameModule.activePlayer);
+    } else if (startGameModule.activePlayer[0] === startGameModule.players[1]) {
+      startGameModule.players[1].addMoveToBoard(`${event.target.id}`);
+      event.target.textContent = startGameModule.players[1].playerSymbol;
+      startGameModule.activePlayer[0] = startGameModule.players[0];
+      // console.log(gameBoardMap.assignedSquares, startGameModule.activePlayer);
+    }
+  }
+  return { playerMove };
+})();
+
+const squares = document.querySelectorAll(".boardGrid__square");
+
+squares.forEach((square) => {
+  square.addEventListener("click", gameBoardController.playerMove);
+});
